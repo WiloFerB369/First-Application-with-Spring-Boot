bb2513148eca097d90b08d291b236c8210c6c81e
WiloFerB369 <wilmer.salamanca@vasslatam.com>
2022-03-18 11:58:34 -0500

    I finished my first application with Spring

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/WiloFerB")
+    @GetMapping("/")
     public String index(){
         return "Greetings from Spring Boot!";
     }
